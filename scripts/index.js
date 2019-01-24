4682f997a924d669d39e0672feba39f6ab95b0b1
Jordan Sandberg <jordansandberg1@gmail.com>
2019-01-23 17:02:52 -0800

    wrote index script, templates and stylesheet

---
+++ scripts/index.js
@@ -0,0 +1,13 @@
+// Test electron application file
+
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  // Create the browser window.
+  win = new BrowserWindow({ width: 800, height: 600 })
+
+  // and load the index.html of the app.
+  win.loadFile('../templates/index.html')
+}
+
+app.on('ready', createWindow)
