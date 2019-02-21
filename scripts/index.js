cb52051a8d45688b794ca4d4f3d70047b6d93dca
Jordan Sandberg <jordansandberg1@gmail.com>
2019-02-21 14:36:06 -0800

    began making click events, electron not recognizing jQuery

--- scripts/index.js
+++ scripts/index.js
@@ -5,6 +5,7 @@ const Application = require('spectron').Application;
 const path = require('path');
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
+const {ipcMain} = require('electron');
 
 // // create path for spectron to run app from
 // var electronPath = path.join(__dirname, '..', 'node_modules', '.bin', 'electron');
@@ -27,16 +28,53 @@ const chaiAsPromised = require('chai-as-promised');
 //   chai.should();
 //   chai.use(chaiAsPromised);
 // });
+let mainWindow;
+let secondWindow;
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({ width: 800, height: 600 })
+  mainWindow = new BrowserWindow({ 
+    width: 800, 
+    height: 600,
+    resizable: true, 
+  })
 
+  secondWindow = new BrowserWindow({
+    width: 800, 
+    height: 600,
+    resizable: true,
+    show: false 
+  })
   // and load the index.html of the app.
-  win.loadFile('templates/index.html')
-
-  // Open the DevTools.
-  win.webContents.openDevTools()
+  mainWindow.loadFile('templates/index.html')
+  secondWindow.loadFile('templates/red.html')
+  
+  // // Open the DevTools.
+  mainWindow.webContents.openDevTools()
 }
 
+ipcMain.on('changeRed', (event, arg) => {
+  secondWindow.show()
+})
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null) {
+    createWindow()
+  }
+})
