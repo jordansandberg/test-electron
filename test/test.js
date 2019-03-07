347c9e308600d746b6d81f903f092489a813add4
jordansandberg <jordansandberg1@gmail.com>
2019-03-06 17:43:57 -0800

    still broke dog

--- test/test.js
+++ test/test.js
@@ -3,6 +3,10 @@ const path = require('path');
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
 
+// init chai
+chai.should();
+chai.use(chaiAsPromised)
+
 // point to the Electron node module
 var electronPath = path.join(__dirname, '..', 'node_modules', '.bin', 'electron');
 
@@ -13,34 +17,59 @@ if (process.platform === 'win32') {
 var appPath = path.join(__dirname, '..');
 
 console.log(appPath);
+console.log(electronPath)
 
 //creates the application for spectron
 var app = new Application({
     path: electronPath,
-    args: [appPath]
+    args: [appPath],
+    env: {
+        ELECTRON_ENABLE_LOGGING: true,
+        ELECTRON_ENABLE_STACK_DUMPING: true,
+        NODE_ENV: "development"
+    },
+    startTimeout: 20000
 });
 
 // set up chai-as-promised and start app
-global.before(function () {
-    chai.should();
-    chai.use(chaiAsPromised);
-    return app.start();
-});
+// global.before(function (done) {
+//     chaiAsPromised.transferPromiseness = app.transferPromiseness;
+// });
 
 // describe() are for grouping; test suites.
 describe("Window-change",function(){
-    // it() are test cases
+    // run before each test
+    beforeEach(function () {
+        this.timeout(20000)
+        return app.start();
+    });
 
-    it('should turn the background red', function(done){
-
-        click('button*=Red');
-        return $('.body').should.have.css('background-color', 'red');
-    })
-});
-
-// teardown
-after(function() {
-    if(app && app.isRunning()){
+    //run after each test
+    afterEach(function() {
         return app.stop();
-    }
+    });
+
+    // it() are test cases
+    it('should turn the background red', function(done){
+        app.client.element('#red')
+            .click()
+            .then(app.client.element('body').getCSSProperty('background-color'))
+            .then(body => {
+                console.log(body)
+                body.getCSSProperty('background-color')
+            }).then(bgColor => {
+                console.log(bgColor)
+                chai.assert.equal(bgColor.value, 'red', 'background color should be red')
+                done()
+            })
+            .catch(done)
+    });
+
+    // it('should turn the background green', function(done){
+    //     return false
+    // })
+
+    // it('should turn the background blue', function(done){
+    //     return false
+    // })
 });
